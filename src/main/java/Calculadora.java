333d7b1be2c9eb16310717097985215072262821
Paula Arroyo <p.arroyosalvo@gmail.com>
2023-08-12 16:43:41 -0400

    Desafio Calculadora II

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Calculadora {
+public class Calculadora {
+
+    public Calculadora(){
+
+    }
+
+    public double sumar(double a, double b){
+        return a+b;
+    }
+
+    public double restar(double a, double b){
+        return a-b;
+    }
+
+    public double multiplicar(double a, double b){
+        return a*b;
+    }
+
+    public Double dividir( double a, double b){
+            Double result = a / b;
+            if(result.isInfinite()){
+                return null;
+            }
+            return result;
+    }
 }
--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class CalculadoraTest {
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculadoraTest {
+    Calculadora calculadora = new Calculadora();
+    @Test
+    public void sumarTest(){
+    double result = calculadora.sumar(2,2);
+    assertEquals(4,result,0);
+    }
+    @Test
+    public void restarTest(){
+        double result = calculadora.restar(5,2);
+        assertEquals(3,result,0);
+    }
+    @Test
+    public void multiplicarTest(){
+        double result = calculadora.multiplicar(5,2);
+        assertEquals(10,result,0);
+    }
+    @Test
+    public void dividirTest(){
+        double result1 = calculadora.dividir(5,2);
+        assertEquals(2.5,result1,0);
+        Double result2 = calculadora.dividir(5,0);
+        assertEquals(null,result2);
+    }
 }
